00b28de0f28cc451d29b1c8c397df4b5e2594cc4
DanubeRS <DanubeRS@gmail.com>
2013-07-31 11:30:13 +1000

    Initial Commit

---
+++ TreeEase/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutoCAD
+{
+    class Tree {
+
+            public float xPos {get; set; }
+            public float yPos {get; set; }
+            public string type {get; set; }
+            public float dia {get; set; }
+            public int height {get; set; }
+            public int spread {get; set; }
+    
+    }
+}
